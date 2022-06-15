284d50f39c3eb4c2499b2853e6706ef71cb7b936
Bektur13 <duyshembekov04@mail.ru>
2022-06-15 23:32:38 +0600

    added search

--- src/App.js
+++ src/App.js
@@ -28,18 +28,25 @@ function App() {
       .then(response => response.json())
       .then(json => setPosts(json))
   })
+  const [searchText, setSearchText] = useState('');
 
+  const filteredPosts = posts.filter(posts => {
+    return posts.title.toLowerCase().includes(searchText.toLowerCase())
+  })
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  
 
   return (
     <BrowserRouter>
       <div className="App">
         <div className='container'>
           <Header />
+          <input className='app__input' placeholder='Введите название...' onChange={(e) => setSearchText(e.target.value)}/>
           <Routes>
             <Route path='/' element={<PostTable posts={currentPosts} postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>} />
             <Route path='users' element={<Users users={users} />} />
